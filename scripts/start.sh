a3e002d6539689bbf652fcf84700e6edc6dac766
Edu Wass <eduwass@gmail.com>
2017-04-10 17:02:10 +0200

    modify start.sh to wait for package.json

--- scripts/start.sh
+++ scripts/start.sh
@@ -6,12 +6,15 @@ if [ ! -z "$DEBS" ]; then
  apt-get install -y $DEBS
 fi
 
-# Dont pull code down if the .git folder exists
-if [ -f "/usr/src/app/package.json" ];then
-  # Code present, start service
-  cd /usr/src/app
-  # Run any commands passed by env
-  eval $STARTUP_COMMANDS
-  # Start!
-  npm start
-fi
+until [ -f /usr/src/app/package.json ]
+do
+  sleep 300 # 5 minutes
+done
+
+echo "package.json found"
+# Code present, start service
+cd /usr/src/app
+# Run any commands passed by env
+eval $STARTUP_COMMANDS
+# Start!
+npm start
